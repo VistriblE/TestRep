2713412cbadfbdceca905081a4c7fd8451306b1f
VistriblE <anischenkomisha@gmail.com>
2018-02-27 16:10:09 +0300

    AAAAAA

--- script.js
+++ script.js
@@ -28,19 +28,19 @@ const operators = [
 
 
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
 function getStrictRandom() {
     const min = -10;    
     const max = 10;
-    const num = getRandomNumber(min, max);
+    const num = getRandomNumber(min, max - 1);
 
     return num == 0 ? max : num;
 }
 
 while (true) {
-    const operator = getRandomNumber(0, 4);
+    const operator = getRandomNumber(0, 3);
     const topValue = getStrictRandom();
     const bottomValue = getStrictRandom();
     const strBottom = bottomValue < 0 ? `(${bottomValue})` : `${bottomValue}`; 
